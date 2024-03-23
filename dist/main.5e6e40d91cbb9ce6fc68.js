3af31fdf3160ea6d190e5ddc67239fc61ad3f000
Maksim Zhatkin <maksim.zhatkin@gmail.com>
2024-03-23 23:00:02 +0300

    final boss

--- dist/main.5e6e40d91cbb9ce6fc68.js
+++ dist/main.5e6e40d91cbb9ce6fc68.js
@@ -25,27 +25,26 @@ controls.forEach(control => {
 /***/ 50:
 /***/ (() => {
 
-const feedbackDialog = document.querySelector('.js-dialog-feedback');
-const openFeedbackDialog = document.querySelectorAll('.js-open-dialog-feedback');
-const closeFeedbackDialog = document.querySelectorAll('.js-close-dialog-feedback');
-const orderCallDialog = document.querySelector('.js-dialog-order-call');
-const openOrderCallDialog = document.querySelectorAll('.js-open-dialog-order-call');
-const closeOrderCallDialog = document.querySelectorAll('.js-close-dialog-order-call');
 function enableBodyScrolling() {
   const scrollY = document.body.style.top;
   document.body.style.position = '';
   document.body.style.top = '';
+  document.body.style.paddingInlineEnd = '';
   window.scrollTo(0, parseInt(scrollY || '0') * -1);
 }
 function disableBodyScrolling(scrollY) {
   document.body.style.position = 'fixed';
   document.body.style.top = `-${scrollY}px`;
+  document.body.style.paddingInlineEnd = '15px';
   document.body.style.left = `${window.innerWidth > 1220 ? Math.abs(window.innerWidth / 2 - 1440 / 2) : 0}px`;
 }
+
+// Function to initialize a modal dialog
 function initModal(dialogNode, closeButtons, openButtons) {
   dialogNode.addEventListener('click', e => {
     let rect = e.target.getBoundingClientRect();
     if (rect.left > e.clientX || rect.right < e.clientX || rect.top > e.clientY || rect.bottom < e.clientY) {
+      dialogNode.setAttribute('aria-hidden', 'true');
       dialogNode.close();
       enableBodyScrolling();
     }
@@ -54,16 +53,24 @@ function initModal(dialogNode, closeButtons, openButtons) {
     item.addEventListener('click', () => {
       disableBodyScrolling(window.scrollY);
       dialogNode.showModal();
+      dialogNode.setAttribute('aria-hidden', 'false');
     });
   });
   closeButtons.forEach(item => {
     item.addEventListener('click', () => {
       enableBodyScrolling();
       dialogNode.close();
+      dialogNode.setAttribute('aria-hidden', 'true');
     });
   });
 }
+const feedbackDialog = document.querySelector('.js-dialog-feedback');
+const openFeedbackDialog = document.querySelectorAll('.js-open-dialog-feedback');
+const closeFeedbackDialog = document.querySelectorAll('.js-close-dialog-feedback');
 initModal(feedbackDialog, closeFeedbackDialog, openFeedbackDialog);
+const orderCallDialog = document.querySelector('.js-dialog-order-call');
+const openOrderCallDialog = document.querySelectorAll('.js-open-dialog-order-call');
+const closeOrderCallDialog = document.querySelectorAll('.js-close-dialog-order-call');
 initModal(orderCallDialog, closeOrderCallDialog, openOrderCallDialog);
 
 /***/ }),
@@ -9528,4 +9535,4 @@ var accordion = __webpack_require__(178);
 
 /******/ })()
 ;
-//# sourceMappingURL=main.6de5190f7a5542476197.js.map
+//# sourceMappingURL=main.5e6e40d91cbb9ce6fc68.js.map
--- src/scripts/dialog.js
+++ src/scripts/dialog.js
@@ -1,30 +1,19 @@
-const feedbackDialog = document.querySelector('.js-dialog-feedback')
-const openFeedbackDialog = document.querySelectorAll('.js-open-dialog-feedback')
-const closeFeedbackDialog = document.querySelectorAll(
-  '.js-close-dialog-feedback'
-)
-
-const orderCallDialog = document.querySelector('.js-dialog-order-call')
-const openOrderCallDialog = document.querySelectorAll(
-  '.js-open-dialog-order-call'
-)
-const closeOrderCallDialog = document.querySelectorAll(
-  '.js-close-dialog-order-call'
-)
-
 function enableBodyScrolling() {
   const scrollY = document.body.style.top
   document.body.style.position = ''
   document.body.style.top = ''
+  document.body.style.paddingInlineEnd = ''
   window.scrollTo(0, parseInt(scrollY || '0') * -1)
 }
 
 function disableBodyScrolling(scrollY) {
   document.body.style.position = 'fixed'
   document.body.style.top = `-${scrollY}px`
+  document.body.style.paddingInlineEnd = '15px'
   document.body.style.left = `${window.innerWidth > 1220 ? Math.abs(window.innerWidth / 2 - 1440 / 2) : 0}px`
 }
 
+// Function to initialize a modal dialog
 function initModal(dialogNode, closeButtons, openButtons) {
   dialogNode.addEventListener('click', (e) => {
     let rect = e.target.getBoundingClientRect()
@@ -35,6 +24,7 @@ function initModal(dialogNode, closeButtons, openButtons) {
       rect.top > e.clientY ||
       rect.bottom < e.clientY
     ) {
+      dialogNode.setAttribute('aria-hidden', 'true')
       dialogNode.close()
       enableBodyScrolling()
     }
@@ -44,6 +34,7 @@ function initModal(dialogNode, closeButtons, openButtons) {
     item.addEventListener('click', () => {
       disableBodyScrolling(window.scrollY)
       dialogNode.showModal()
+      dialogNode.setAttribute('aria-hidden', 'false')
     })
   })
 
@@ -51,9 +42,24 @@ function initModal(dialogNode, closeButtons, openButtons) {
     item.addEventListener('click', () => {
       enableBodyScrolling()
       dialogNode.close()
+      dialogNode.setAttribute('aria-hidden', 'true')
     })
   })
 }
 
+const feedbackDialog = document.querySelector('.js-dialog-feedback')
+const openFeedbackDialog = document.querySelectorAll('.js-open-dialog-feedback')
+const closeFeedbackDialog = document.querySelectorAll(
+  '.js-close-dialog-feedback'
+)
 initModal(feedbackDialog, closeFeedbackDialog, openFeedbackDialog)
+
+const orderCallDialog = document.querySelector('.js-dialog-order-call')
+const openOrderCallDialog = document.querySelectorAll(
+  '.js-open-dialog-order-call'
+)
+const closeOrderCallDialog = document.querySelectorAll(
+  '.js-close-dialog-order-call'
+)
+
 initModal(orderCallDialog, closeOrderCallDialog, openOrderCallDialog)
